bfb3a9010003cdb78b4866da16a6e6148461daec
garronej <joseph.garrone@protonmail.com>
2025-05-18 19:54:16 +0200

    Setup build script

---
+++ scripts/build.ts
@@ -0,0 +1,78 @@
+import * as child_process from "child_process";
+import * as fs from "fs";
+import { join as pathJoin } from "path";
+import { getThisCodebaseRootDirPath } from "./tools/getThisCodebaseRootDirPath";
+import { z } from "zod";
+import { assert, type Equals } from "tsafe/assert";
+import { id } from "tsafe/id";
+import { transformCodebase } from "./tools/transformCodebase";
+
+const distDirPath = pathJoin(getThisCodebaseRootDirPath(), "dist");
+
+fs.rmSync(distDirPath, { recursive: true, force: true });
+
+child_process.execSync("npx tsc");
+
+for (const dirBasename of ["src", "keycloak-theme"]) {
+    transformCodebase({
+        srcDirPath: pathJoin(getThisCodebaseRootDirPath(), dirBasename),
+        destDirPath: pathJoin(distDirPath, dirBasename)
+    });
+}
+
+type ParsedPackageJson = {
+    name: string;
+    version: string;
+    description: string;
+    repository: Record<string, string>;
+    author: string;
+    license: string;
+    keywords: string[];
+    homepage: string;
+    dependencies: Record<string, string>;
+    peerDependencies: Record<string, string>;
+};
+
+const zParsedPackageJson = (() => {
+    type TargetType = ParsedPackageJson;
+
+    const zTargetType = z.object({
+        name: z.string(),
+        version: z.string(),
+        description: z.string(),
+        repository: z.record(z.string(), z.string()),
+        author: z.string(),
+        license: z.string(),
+        keywords: z.array(z.string()),
+        homepage: z.string(),
+        dependencies: z.record(z.string(), z.string()),
+        peerDependencies: z.record(z.string(), z.string())
+    });
+
+    type InferredType = z.infer<typeof zTargetType>;
+
+    assert<Equals<ParsedPackageJson, InferredType>>;
+
+    return id<z.ZodType<TargetType>>(zTargetType);
+})();
+
+const parsedPackageJson_root = zParsedPackageJson.parse(
+    JSON.parse(fs.readFileSync(pathJoin(getThisCodebaseRootDirPath(), "package.json")).toString("utf8"))
+);
+
+fs.writeFileSync(
+    pathJoin(distDirPath, "package.json"),
+    Buffer.from(
+        JSON.stringify(
+            {
+                ...parsedPackageJson_root,
+                publishConfig: {
+                    access: "public"
+                }
+            },
+            null,
+            2
+        ),
+        "utf8"
+    )
+);
