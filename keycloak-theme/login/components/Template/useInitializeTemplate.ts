1cd3faa40e9a3c70706497289cd942de2d195027
garronej <joseph.garrone@protonmail.com>
2025-06-07 19:37:02 +0200

    fix imports

--- keycloak-theme/login/components/Template/useInitializeTemplate.ts
+++ keycloak-theme/login/components/Template/useInitializeTemplate.ts
@@ -8,8 +8,8 @@
 /* eslint-disable */
 
 import { useEffect } from "react";
-import { useInsertScriptTags } from "../../../tools/useInsertScriptTags";
-import { useInsertLinkTags } from "../../../tools/useInsertLinkTags";
+import { useInsertScriptTags } from "@keycloakify/keycloak-login-ui/tools/useInsertScriptTags";
+import { useInsertLinkTags } from "@keycloakify/keycloak-login-ui/tools/useInsertLinkTags";
 import { useKcClsx } from "@keycloakify/keycloak-login-ui/useKcClsx";
 // NOTE: If you are in a Vite project you can use `import.meta.env.BASE_URL` directly, this is a shim to support Webpack.
 import { BASE_URL } from "@keycloakify/keycloak-login-ui/BASE_URL";
